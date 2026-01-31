0f17bf6f2198d46bfdf7edde6b852a7d15077fa2
Rubenslourenco <alvesrubens926@gmail.com>
2026-01-31 20:00:33 -0300

    feat: add Sequelize configuration and model initialization

--- backend-Docker/src/config/database.js
+++ backend-Docker/src/config/database.js
@@ -1,3 +1,14 @@
 export default {
-    
-}
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'usersdb',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,  
+    }
+}       
+        
+        
